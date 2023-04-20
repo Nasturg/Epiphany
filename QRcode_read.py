2255d86d4ca42b0a683cab95743ffe8c304501f6
Nasturg <109023704+Nasturg@user.noreply.github.com>
2023-04-20 15:07:46 +0300

    Write all QRcode_read.py

--- QRcode_read.py
+++ QRcode_read.py
@@ -12,6 +12,10 @@ qr_create = 'ya.png'
 qr_format = 'png'
 qr_format_old = 'jpg'
 
+color = (255, 255, 255)  # Белый цвет в формате BGR
+thickness = 0  # Толщина рамки в пикселях
+border_size = 2  # Размер рамки в пикселях
+
 '''
 # Генерация QR кода
 qr = qrcode.QRCode(
@@ -41,9 +45,32 @@ for i in range(num_images):  # Чтение QR кода
 
     data, bbox, clear_qrcode = detector.detectAndDecode(img_qrcode)
 
+    # '''
+    if i < 11:
+        # Добавление рамки к изображению
+        img_with_border = cv2.copyMakeBorder(clear_qrcode, border_size, border_size, border_size, border_size,
+                                             cv2.BORDER_CONSTANT, value=color)
+
+        # Определение координат рамки
+        x1, y1 = border_size - 1, border_size - 1  # Верхний левый угол
+        x2, y2 = clear_qrcode.shape[1] + border_size, clear_qrcode.shape[
+            0] + border_size  # Нижний правый угол
+
+        # Добавление рамки к изображению
+        img_with_border = cv2.rectangle(img_with_border, (x1, y1), (x2, y2), color, thickness)
+
+        clear_qrcode = img_with_border
+    # '''
+
     # cv2.imshow('frame', clear_qrcode)
 
-    cv2.imwrite(path_qr_clean + str(i + 1) + '.' + qr_format, clear_qrcode)
+    # увеличиваем размер изображения в два раза
+    new_size = (clear_qrcode.shape[1] * 3, clear_qrcode.shape[0] * 3)
+    # Увеличение
+    resized_img = cv2.resize(clear_qrcode, new_size, interpolation=cv2.INTER_NEAREST)
+    # resized_img = cv2.resize(resized, (75, 75))
+
+    cv2.imwrite(path_qr_clean + str(i + 1) + '.' + qr_format, resized_img)
     print(path_qr_clean + str(i + 1) + '.' + qr_format)
 
     # print(data)
